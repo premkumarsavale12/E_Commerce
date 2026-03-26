e13ce51687c2d020608d2a33657aa98fb498dccf
Premkumar Savale <premsavle112@gmail.com>
2026-03-26 12:34:59 +0530

    Added Code

--- frontend/frontend/src/pages/Section_1.jsx
+++ frontend/frontend/src/pages/Section_1.jsx
@@ -31,27 +31,55 @@ const Section_1 = () => {
     return (
 
         <>
-            <div className="max-w-[1300px] mx-auto px-4 sm:px-6 py-8">
-     {
+            <div className="max-w-[1300px] max-h-[1200px] mx-auto px-4 sm:px-6 py-10">
+                {
+                    data.map((item, index) => {
+                        return (
+                            <div
+                                key={index}
+                                className="flex flex-col md:flex-row items-center justify-between bg-gray-100 p-8 rounded-lg"
+                            >
 
-                        data.map((item, index) => {
-                            console.log(item);
+                                {/* LEFT CONTENT */}
+                                <div className="md:w-1/2 space-y-4">
+                                    <h1 className="text-4xl font-bold">
+                                        {item.Heading}
+                                    </h1>
 
-                            return (
-                                <div key={index}>
-                                    <img src={item.Image} alt=""
+                                    <h2 className="text-xl text-gray-700">
+                                        {item.Sub_Heading}
+                                    </h2>
 
+                                    <p className="text-gray-600">
+                                        {item.Description}
+                                    </p>
+
+                                    <p className="text-lg font-semibold">
+                                        {item.Sub_Description}
+                                    </p>
+
+                                    <button className="bg-black text-white px-6 py-2 rounded-md hover:bg-gray-800">
+                                        {item.Button}
+                                    </button>
+                                </div>
+
+                                {/* RIGHT IMAGE */}
+                                <div className="md:w-1/2 mt-6 md:mt-0 flex justify-center">
+                                    <img
+                                        src={`http://localhost:5000/uploads/${item.Image}`}
+                                        alt=""
+                                        className="w-full max-w-md object-contain"
                                     />
                                 </div>
-                            );
-                        })
 
-                    }
+                            </div>
+                        );
+                    })
+                }
+            </div>
 
-                </div>
 
 
-          
         </>
     )
 }
