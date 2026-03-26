37df5dfffefa9b0e345e9866a9b4322bcb781c93
Premkumar Savale <premsavle112@gmail.com>
2026-03-26 10:53:29 +0530

    Add Code Chnage

--- backend/model/hero_section.js
+++ backend/model/hero_section.js
@@ -3,9 +3,35 @@ const mongoose = require('mongoose');
 
 const Hero_Section = new mongoose.Schema({
 
+    Heading: {
+        type: String,
+
+    },
+
+    Sub_Heading: {
+        type: String,
+      
+    },
+    Description: {
+        type: String,
+        
+
+    },
+
+    Sub_Description: {
+        type: String,
+  
+
+    },
+
+    Button: {
+        type: String,
+     
+    },
+
     Image: {
         type: String,
-        required: true
+      
     }
 })
 
